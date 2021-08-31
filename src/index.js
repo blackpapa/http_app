5ae08032daebe504e86ffeabd655a727630528b9
Andy Chen <andycjx8@gmail.com>
2021-08-31 11:33:25 +1000

    Install sentry and use it to log error

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Sentry.init({
+  dsn: "https://d76fd96adda8459dadf07249de42b5a0@o494006.ingest.sentry.io/5938301",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- src/services/httpservice.js
+++ src/services/httpservice.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as Sentry from "@sentry/react";
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
@@ -7,7 +8,7 @@ axios.interceptors.response.use(null, (error) => {
     error.response.status <= 500;
 
   if (!expectedError) {
-    console.log("Logging the error", error);
+    Sentry.captureException(error);
     alert("Unexpected error occur");
   }
 
